897fd525784e666822c321a5e551bd0229952cab
Alex <89754011+alexxcyb@users.noreply.github.com>
2021-10-16 09:37:49 +0200

    Update ex-1.ino

--- ex-1.ino
+++ ex-1.ino
@@ -1,25 +1,24 @@
-# define pinLedGreen 11;
-# define pinLedYellow 12;
-# define pinLedRed 10;
+# define Green 11
+# define Yellow 12
+# define Red 10
 
 void setup() {
-  pinMode(pinLedGreen, OUTPUT);
-  pinMode(pinLedYellow, OUTPUT);
-  pinMode(pinLedRed, OUTPUT);
+  pinMode(Green, OUTPUT);
+  pinMode(Yellow, OUTPUT);
+  pinMode(Red, OUTPUT);
 }
 
 void loop() {
-  int greenTime  = 4000;
-  int yellowTime = 1000;
-  int redTime = 5000;
-  
-  digitalWrite(pinLedGreen, 1);   
-  delay(greenTime);               
-  digitalWrite(pinLedGreen, 0);     
-  digitalWrite(pinLedYellow, 1);     
-  delay(yellowTime);
-  digitalWrite(pinLedYellow, 0);     
-  digitalWrite(pinLedRed, 1);     
-  delay(redTime);
-  digitalWrite(pinLedRed, 0);
+  digitalWrite(Green, 1);
+  digitalWrite(Yellow, 0);
+  digitalWrite(Green, 0);
+  delay(Green);
+  digitalWrite(Green, 0);
+  digitalWrite(Yellow, 1);
+  digitalWrite(Green, 0);
+  delay(Yellow);
+  digitalWrite(Green, 0);
+  digitalWrite(Yellow, 0);
+  digitalWrite(Green, 1);
+  delay(Red);
 }
