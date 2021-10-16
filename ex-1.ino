a0c6edfdc620b03bd0f61e8b9d4dd07e1f7b4ee1
Alex <89754011+alexxcyb@users.noreply.github.com>
2021-10-16 09:47:47 +0200

    Update ex-1.ino

--- ex-1.ino
+++ ex-1.ino
@@ -22,3 +22,32 @@ void loop() {
   digitalWrite(Green, 1);
   delay(Red);
 }
+
+
+
+/* 
+# define Green 11
+# define Yellow 12  (parte dichiarativa)
+# define Red 10
+
+void setup() {
+  pinMode(Green, OUTPUT);
+  pinMode(Yellow, OUTPUT);
+  pinMode(Red, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(Green, 1);
+  digitalWrite(Yellow, 0);
+  digitalWrite(Green, 0);
+  delay(Green);
+  digitalWrite(Green, 0);
+  digitalWrite(Yellow, 1);
+  digitalWrite(Green, 0);
+  delay(Yellow);
+  digitalWrite(Green, 0);
+  digitalWrite(Yellow, 0);
+  digitalWrite(Green, 1);
+  delay(Red);
+}
+*/
